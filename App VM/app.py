bb732119e41cb238c12fcb60cffe27134c9a197f
sajaldebnath <sajaldebnath@users.noreply.github.com>
2023-03-16 13:13:04 +0530

    Add files via upload

---
+++ App VM/app.py
@@ -0,0 +1,259 @@
+#!/usr/bin python
+
+"""
+#
+# Program to get data from remote DB server and show in the frontend
+# This script defines all the routes to be accessed by frontend web server
+# Author Sajal Debnath <sdebnath@vmware.com><debnathsajal@gmail.com>
+#
+"""
+# Importing the Modules
+from flask import Flask, render_template, request, jsonify
+import requests
+# Defining the Database details
+import time
+import json
+
+
+db_fqdn = "10.123.10.187"  # Modify the value with the actual database server
+api_url_base = "http://" + db_fqdn + "/employee"
+
+# Defining the app details
+# Init app
+app = Flask(__name__)
+
+app.config["DEBUG"] = True
+
+################### Define functions and routes ##########################
+
+# Get all the employees data
+@app.route('/')
+def employees():#
+    api_url = "{0}".format(api_url_base)
+
+    response = ''
+    while response == '':
+        try:
+            response = requests.get(api_url, headers=header, verify=False)
+            break
+        except:
+            for i in range(3):
+                print("Connection refused by the server..")
+                print("Let's wait for 5 seconds and try again")
+                time.sleep(5)
+                print("Trying again...")
+                continue
+    
+    #print(response)
+    if response.status_code == 200:
+         json_data_all = response.json()
+         json_data = json_data_all["data"]
+         employee_data = json_data[0]
+         #print(json_data[0])
+         return render_template('homepage.html',employee_data=employee_data)
+    else:
+         print("Could not get employee list")
+         return "Could not get employee list"
+
+
+# Create a new employee record
+@app.route('/employee', methods=["POST"]) #,"GET"])  
+def single_employee():
+    api_url = "{0}".format(api_url_base)
+    headers = {'content-type': 'application/json'}
+
+    if request.method == 'POST':
+        emp_id = request.form['emp_id']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        #email = first_name + '.' + last_name + 'acme.com'  # Email auto-generated
+        email = request.form['email']
+        ph_no = request.form['ph_no']
+        home_addr = request.form['home_addr']
+        st_addr = request.form['st_addr']
+        gender = request.form['gender']
+        job_type = request.form['job_type']
+        # print(emp_id)
+        # print(first_name)
+        # print(last_name)
+        # print(email)
+        # print(ph_no)
+        # print(home_addr)
+        # print(st_addr)
+        # print(gender)
+        # print(job_type)
+        # print("I am making the request now")
+        request_body = {
+            "emp_id": emp_id,
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": email,
+            "ph_no": ph_no,
+            "home_addr": home_addr,
+            "st_addr": st_addr,
+            "gender": gender,
+            "job_type": job_type
+            }
+        
+        #payload = jsonify(request_body)
+        print("Request Body", request_body)
+        
+        response = ''
+        while response == '':
+            try:
+                response = requests.post(api_url, data = json.dumps(request_body), headers=headers)
+                break
+            except:
+                for i in range(3):
+                    print("Connection refused by the server..")
+                    print("Let's wait for 5 seconds and try again")
+                    time.sleep(5)
+                    print("Trying again...")
+                    continue
+        
+        #print(response)
+        
+        info=response.json()
+        print(info)
+
+
+        print(response.status_code)
+        if response.status_code == 201:
+            json_data_all = response.json()
+            json_data = json_data_all["data"]
+
+            print("Successfully added the record for the employee: {}".format(first_name))
+            return ("Successfully added the record for the employee: {}".format(first_name))
+        else:
+            print("Could not add to the employee record. Error: {}".format(info))
+            
+            return ("Could not add to the employee record. Error: {}".format(info))
+
+
+# GET, UPDATE/Edit, DELETE an existing employee record
+@app.route('/employee/<int:id>', methods=["PUT","GET", "DELETE"])
+def edit_employee_record(id):
+    api_url = "{0}/{1}".format(api_url_base, id)
+    headers = {'content-type': 'application/json'}
+    # response = requests.get(api_url, headers=header, verify=False)
+
+    if request.method == 'PUT':
+        emp_id = request.form['emp_id']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = first_name + '.' + last_name + 'acme.com'  # Email auto-generated
+        ph_no = request.form['ph_no']
+        home_addr = request.form['home_addr']
+        st_addr = request.form['st_addr']
+        gender = request.form['gender']
+        job_type = request.form['job_type']
+        # print(emp_id)
+        # print(first_name)
+        # print(last_name)
+        # print(email)
+        # print(ph_no)
+        # print(home_addr)
+        # print(st_addr)
+        # print(gender)
+        # print(job_type)
+        # print("I am making the edit request now")
+        request_body = {
+            "emp_id": emp_id,
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": email,
+            "ph_no": ph_no,
+            "home_addr": home_addr,
+            "st_addr": st_addr,
+            "gender": gender,
+            "job_type": job_type
+            }
+        
+        #payload = jsonify(request_body)
+        print("Request Body", request_body)
+        response = ''
+        while response == '':
+            try:
+                response = requests.put(api_url, data = json.dumps(request_body), headers=headers)
+                break
+            except:
+                for i in range(3):
+                    print("Connection refused by the server..")
+                    print("Let's wait for 5 seconds and try again")
+                    time.sleep(5)
+                    print("Trying again...")
+                    continue
+        
+        info=response.json()
+        print(info)
+
+
+        print(response.status_code)
+        #if response.status_code == 201:
+        if response.status_code == 200:
+            json_data_all = response.json()
+            json_data = json_data_all["data"]
+
+            print("Successfully edited the record for the employee: {}".format(json_data))
+            return ("Successfully edited the record for the employee: {}".format(json_data))
+        else:
+            print("Could not edit the employee record. Error: {}".format(info))
+            
+            return ("Could not edit the employee record. Error: {}".format(info))
+
+    elif request.method == 'GET':
+        response = ''
+        while response == '':
+            try:
+                response = requests.get(api_url, headers=header, verify=False)
+                break
+            except:
+                for i in range(3):
+                    print("Connection refused by the server..")
+                    print("Let's wait for 5 seconds and try again")
+                    time.sleep(5)
+                    print("Trying again...")
+                    continue
+        
+        #print(response)
+        if response.status_code == 200:
+            json_data_all = response.json()
+            json_data = json_data_all["data"]
+
+            return render_template('homepage.html',json_data=json_data)
+        else:
+            print("Could not get employee list")
+            return "Could not get employee list"
+    
+    elif request.method == 'DELETE':
+        response = ''
+        while response == '':
+            try:
+                response = requests.delete(api_url, headers=header, verify=False)
+                break
+            except:
+                for i in range(3):
+                    print("Connection refused by the server..")
+                    print("Let's wait for 5 seconds and try again")
+                    time.sleep(5)
+                    print("Trying again...")
+                    continue
+        
+        #print(response)
+        if response.status_code == 200:
+            json_data_all = response.json()
+            json_data = json_data_all["data"]
+
+            print("Successfully deleted the record: {}".format(json_data))
+            return ("Successfully deleted the record: {}".format(json_data))
+        else:
+            print("Could not delete employee record")
+            return "Could not delete employee record"
+
+
+
+##### MAIN ##########
+header = {"Content-Type": "application/json", "Accept": "application/json"}
+
+if __name__ == '__main__':
+    app.run()
---
+++ App VM/static/js/script.js
@@ -0,0 +1,181 @@
+// All the JavaScript functions for datatable and other calls are defined here
+//Defining the datatable.
+jQuery(document).ready(function($){
+    $('#myTable').DataTable( {
+        columns: [                  //Defining table structure. Must align with the database collection structure
+            { data: 'emp_id' },
+            { data: 'first_name' },
+            { data: 'last_name' },
+            { data: 'email' },
+            { data: 'ph_no' },
+            { data: 'home_addr' },
+            { data: 'st_addr' },
+            { data: 'gender' },
+            { data: 'job_type' },
+            { data: 'edit'},
+            { data: 'delete'},
+        ],
+        searching: true,
+        //select: "single", // Use this to select a single row and take action on that
+        "autoWidth": false,
+        responsive: true,
+        //fixedColumns: true,
+        fixedHeader: true,
+    } );
+    
+    
+    // Handling the edit function
+    // This will activate when the edit button in the table row clicked
+    $("#myTable").on("click", ".edit-button", function(){  
+        $tr = $(this).closest('tr');
+        // Popup modal when clicking on the Edit icon
+        $(".modal-body div span").text("");
+        $(".emp_id span").text($( $tr[0].cells[0]).text());
+        
+        // Getting the default value of the row items (fields)
+        // document.getElementById("edit-emp_id").value = $('.emp_id', $tr).text();
+        document.getElementById("edit-first_name").value = $('.first_name', $tr).text();
+        document.getElementById("edit-last_name").value = $('.last_name', $tr).text();
+        document.getElementById("edit-ph_no").value = $('.ph_no', $tr).text();
+        document.getElementById("edit-home_addr").value = $('.home_addr', $tr).text();
+        document.getElementById("edit-st_addr").value = $('.st_addr', $tr).text();
+        document.getElementById("edit-gender").value = $('.gender', $tr).text();
+        document.getElementById("edit-job_type").value = $('.job_type', $tr).text();
+        
+        $("#myEditModal").modal("show");  // Showing the myEditModal defined in the modals.html
+
+        $('#edit_record_button').on('click', function(){  
+            $("#myEditModal").modal("hide");
+
+            // Getting the updated value of the row items (fields)
+            var emp_id = $( $tr[0].cells[0]).text() // non-editable, so taking the default value of emp_id
+            var first_name = $('#edit-first_name').val(); 
+            var last_name = $('#edit-last_name').val();
+            var email = first_name + '.' + last_name + '@acme.com';
+            var ph_no = $('#edit-ph_no').val();
+            var home_addr = $('#edit-home_addr').val(); 
+            var st_addr = $('#edit-st_addr').val(); 
+            var gender = $('#edit-gender').val(); 
+            var job_type = $('#edit-job_type').val(); 
+            //alert("Reached till this end. Got the new data");
+
+            // Fields mentioned below cannot be empty
+            if(emp_id != '' && first_name != '' && last_name != '' && ph_no != '' && job_type != '')  
+            {  
+                $.ajax({  
+                    url:"/employee/"+ $( $tr[0].cells[0]).text(),  
+                    // method:"POST",  
+                    method:"PUT",
+                    data: {emp_id:emp_id, first_name:first_name, last_name:last_name, email:email, ph_no:ph_no, home_addr:home_addr, st_addr:st_addr, gender:gender,job_type:job_type},  
+                    success:function(data)  
+                    {  
+                        alert(data);  
+                        if(data == 'No-data')  
+                        {  
+                            alert("Invalid Records!");  
+                        }  
+                        else
+                        {  
+                            $('#myEditModal').hide();  
+                            location.reload();  
+                        }  
+                    }  
+                });  
+            }  
+            else
+            {  
+                    alert("All Fields are required");  
+            }    
+            
+        });  
+        //alert("You want to edit: Record ID " + $('.id', $tr).text() + " & Artist: " + $('.artist', $tr).text());     
+        //You can use this info and set it to the inputs with javascript: $("edit_category_modal input[type='text']").val($('.category-name', $tr).text()) for example;
+    });
+
+    // Handling the delete function
+
+    $("#myTable").on("click", ".delete-button", function(){
+        $tr = $(this).closest('tr');
+        // Popup modal when clicking on the delete icon
+        
+        $(".modal-body div span").text("");
+        $(".emp_id span").text($( $tr[0].cells[0]).text());
+        $(".first_name span").text($('.first_name', $tr).text());
+        $(".last_name span").text($('.last_name', $tr).text());
+        $(".email span").text($('.email', $tr).text());
+        $(".ph_no span").text($('.ph_no', $tr).text());
+        $(".home_addr span").text($('.home_addr', $tr).text());
+        $(".st_addr span").text($('.st_addr', $tr).text());
+        $(".gender span").text($('.gender', $tr).text());
+        $(".job_type span").text($('.job_type', $tr).text());
+        $("#myDeleteModal").modal("show");
+        //alert("You want to edit: Record ID " + $('.id', $tr).text() + " & Artist: " + $('.artist', $tr).text());     
+        //You can use this info and set it to the inputs with javascript: $("edit_category_modal input[type='text']").val($('.category-name', $tr).text()) for example;
+
+        // Delete the Record
+        $('#delete_record_button').on('click', function(){  
+            $("#myDeleteModal").modal("hide");
+            $.ajax({  
+                // url:"/delete/"+ $( $tr[0].cells[0]).text(), 
+                url:"/employee/"+ $( $tr[0].cells[0]).text(), 
+                //method:"POST",  
+                method:"DELETE",
+                success:function(data)  
+                {  
+                    alert(data);  
+                    if(data == 'No-data')  
+                    {  
+                        alert("Invalid Records!");  
+                    }  
+                    else
+                    {  
+                        $('#myDeleteModal').hide();  
+                        location.reload();  
+                    }  
+                }  
+            });  
+            
+        });  
+        
+    });
+
+// Adding an employee information to the database
+    $('#add_record_button').click(function(){  
+        var emp_id = $('#emp_id').val();  
+        var first_name = $('#first_name').val(); 
+        var last_name = $('#last_name').val();
+        var email = first_name + '.' + last_name + '@acme.com';
+        var ph_no = $('#ph_no').val();
+        var home_addr = $('#home_addr').val(); 
+        var st_addr = $('#st_addr').val(); 
+        var gender = $('#gender').val(); 
+        var job_type = $('#job_type').val(); 
+
+
+        if(emp_id != '' && first_name != '' && last_name != '' && ph_no != '' && job_type != '')  
+        {  
+                $.ajax({  
+                    url:"/employee",  
+                    method:"POST",  
+                    data: {emp_id:emp_id, first_name:first_name, last_name:last_name, email:email, ph_no:ph_no, home_addr:home_addr, st_addr:st_addr, gender:gender,job_type:job_type},  
+                    success:function(data)  
+                    {  
+                        alert(data);  
+                        if(data == 'No-data')  
+                        {  
+                            alert("Invalid Records!");  
+                        }  
+                        else
+                        {  
+                            $('#addModal').hide();  
+                            location.reload();  
+                        }  
+                    }  
+                });  
+        }  
+        else
+        {  
+                alert("All Fields are required");  
+        }  
+    }); 
+});
